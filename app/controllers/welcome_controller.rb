30fe7f273acd7f27fda5be48887ed4131cb3494a
Salaz <salaznys@gmail.com>
2017-09-04 11:54:10 -0400

    explorer mode of the daily project

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+    @welcome = " WELCOME !!!"
+  end
+  def show
+    @name = params[:name]
+  end
+
+  def Khaled
+
+  end
+
+  def Coffee
+  end
+
+  # def Bluth
+  # end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'welcome#index'
+  get '/:name' => 'welcome#show'
+  get '/lorem/Khaled' => 'welcome#Khaled'
+  get '/lorem/Cupcake' => 'welcome#Cupcake'
+  get '/lorem/Bluth' => 'welcome#Bluth'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
